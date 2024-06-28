f95a4ba3f6facc984a73601469a9a8f537535988
Olga Ovcharenko <157131948+mazdaolga@users.noreply.github.com>
2024-06-28 13:51:44 +0300

    correction main.js pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,14 @@
-export const params = new URLSearchParams({
-  key: '44502992-491f023f405adfa93e793762c',
-  q: '',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-});
+export function getImages(searchQuery) {
+  const params = new URLSearchParams({
+    key: '44502992-491f023f405adfa93e793762c',
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
 
-export function getImages(url) {
-  return fetch(url).then(response => {
+const userUrl = 'https://pixabay.com/api/?' + params;
+  return fetch(userUrl).then(response => {
     if (!response.ok) throw new Error(response.status);
     return response.json();
   });
--- src/main.js
+++ src/main.js
@@ -1,54 +1,55 @@
-// Описаний у документації
+
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-import { params, getImages } from './js/pixabay-api.js';
+import { getImages } from './js/pixabay-api.js';
 import renderGallery from './js/render-function.js';
+
 const refs = {
   form: document.querySelector('#form-request'),
   gallery: document.querySelector('.gallery'),
   loader: document.querySelector('.loader'),
 };
+
 refs.form.addEventListener('submit', e => {
   e.preventDefault();
   refs.loader.classList.remove('hidden');
   const userText = e.target.query.value.trim();
-  setTimeout(() => {
-    if (!userText) {
-      refs.loader.classList.add('hidden');
-      iziToast.warning({
-        title: 'Attention!',
-        message: 'Search field must be filled',
+
+  if (!userText) {
+    refs.loader.classList.add('hidden');
+    iziToast.warning({
+      title: 'Attention!',
+      message: 'Search field must be filled',
+      messageSize: '16',
+      position: 'topRight',
+      close: false,
+      displayMode: 1,
+    });
+    return;
+  }
+
+  getImages(userText)
+    .then(data => {
+      if (!data.totalHits) {
+      refs.gallery.innerHTML = ''; // Clear the gallery
+        throw new Error('Sorry, there are no images matching your search query. Please try again!');
+      }
+      renderGallery(data, refs.gallery);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error!',
+        message: error.message,
         messageSize: '16',
         position: 'topRight',
         close: false,
         displayMode: 1,
       });
-      return;
-    }
-    params.set('q', userText);
-    const userUrl = 'https://pixabay.com/api/?' + params;
-    getImages(userUrl)
-      .then(data => {
-        console.log(data);
-        if (!data.totalHits) {
-          throw new Error(
-            'Sorry, there are no images matching your search query. Please try again!'
-          );
-        }
-        renderGallery(data, refs.gallery);
-      })
-      .catch(error => {
-        iziToast.error({
-          title: 'Error!',
-          message: error.message,
-          messageSize: '16',
-          position: 'topRight',
-          close: false,
-          displayMode: 1,
-        });
-      });
-    e.target.reset();
-    refs.loader.classList.add('hidden');
-  }, 1000);
+    })
+    .finally(() => {
+      refs.loader.classList.add('hidden');
+    });
+
+  e.target.reset();
 });
+
